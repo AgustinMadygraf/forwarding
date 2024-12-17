d3d6531f1356499f47980c759059bd4be8e950c0
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-12-17 15:58:41 -0300

    refactor: add Bootstrap 5.3.3 dependency and include Bootstrap CSS in main.js; update .gitignore and configure devServer port in vue.config.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ Path: src/main.js
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,8 @@ Este archivo se encarga de configurar el proyecto Vue.js.
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.VUE_APP_PUBLIC_PATH
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  devServer: {
+    port: 8081
+  }
 })
